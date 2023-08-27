2af0f3411e369ec746bf0c4b753b2259fdf9b397
dsjang <dsjang>
2023-08-27 20:38:07 +0900

    no message

--- src/main/java/com/example/configuration/listener/session/CustomHttpSessionEventPublisher.java
+++ src/main/java/com/example/configuration/listener/session/CustomHttpSessionEventPublisher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 dsjang
+ * Copyright 2020 장동선.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
